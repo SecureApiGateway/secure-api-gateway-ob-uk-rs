8ffe6c49c82c6abde7f58093e6897a22d59607db
Matt Wills <matt.wills@forgerock.com>
2021-02-09 16:21:18 +0000

    45 - v3.1.1 to v3.1.3 of Payment API Controllers
    Issue: https://github.com/SecureBankingAcceleratorToolkit/SecureBankingAcceleratorToolkit/issues/45

---
+++ securebanking-openbanking-uk-rs-simulator/src/main/java/com/forgerock/securebanking/openbanking/uk/rs/api/obie/payment/v3_1_1/domesticstandingorders/DomesticStandingOrdersApiController.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright © 2020 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.securebanking.openbanking.uk.rs.api.obie.payment.v3_1_1.domesticstandingorders;
+
+import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.account.FRStandingOrderData;
+import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.payment.FRWriteDomesticStandingOrder;
+import com.forgerock.securebanking.openbanking.uk.error.OBErrorResponseException;
+import com.forgerock.securebanking.openbanking.uk.rs.common.util.VersionPathExtractor;
+import com.forgerock.securebanking.openbanking.uk.rs.persistence.document.payment.FRDomesticStandingOrderPaymentSubmission;
+import com.forgerock.securebanking.openbanking.uk.rs.persistence.repository.IdempotentRepositoryAdapter;
+import com.forgerock.securebanking.openbanking.uk.rs.persistence.repository.payments.DomesticStandingOrderPaymentSubmissionRepository;
+import com.forgerock.securebanking.openbanking.uk.rs.service.standingorder.StandingOrderService;
+import com.forgerock.securebanking.openbanking.uk.rs.validator.PaymentSubmissionValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import uk.org.openbanking.datamodel.account.Meta;
+import uk.org.openbanking.datamodel.payment.OBWriteDataDomesticStandingOrderResponse3;
+import uk.org.openbanking.datamodel.payment.OBWriteDomesticStandingOrder3;
+import uk.org.openbanking.datamodel.payment.OBWriteDomesticStandingOrderResponse3;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.forgerock.securebanking.openbanking.uk.rs.api.obie.payment.FRStandingOrderDataFactory.createFRStandingOrderData;
+import static com.forgerock.securebanking.openbanking.uk.rs.api.obie.payment.LinksHelper.createDomesticStandingOrderPaymentLink;
+import static com.forgerock.securebanking.openbanking.uk.rs.converter.payment.FRSubmissionStatusConverter.toOBExternalStatus1Code;
+import static com.forgerock.securebanking.openbanking.uk.rs.converter.payment.FRWriteDomesticStandingOrderConsentConverter.toOBDomesticStandingOrder3;
+import static com.forgerock.securebanking.openbanking.uk.rs.converter.payment.FRWriteDomesticStandingOrderConverter.toFRWriteDomesticStandingOrder;
+import static com.forgerock.securebanking.openbanking.uk.rs.persistence.document.payment.FRSubmissionStatus.INITIATIONPENDING;
+
+@Controller("DomesticStandingOrdersApiV3.1.1")
+@Slf4j
+public class DomesticStandingOrdersApiController implements DomesticStandingOrdersApi {
+
+    private final DomesticStandingOrderPaymentSubmissionRepository standingOrderPaymentSubmissionRepository;
+    private final PaymentSubmissionValidator paymentSubmissionValidator;
+    private final StandingOrderService standingOrderService;
+
+    public DomesticStandingOrdersApiController(
+            DomesticStandingOrderPaymentSubmissionRepository standingOrderPaymentSubmissionRepository,
+            PaymentSubmissionValidator paymentSubmissionValidator,
+            StandingOrderService standingOrderService) {
+        this.standingOrderPaymentSubmissionRepository = standingOrderPaymentSubmissionRepository;
+        this.paymentSubmissionValidator = paymentSubmissionValidator;
+        this.standingOrderService = standingOrderService;
+    }
+
+    @Override
+    public ResponseEntity<OBWriteDomesticStandingOrderResponse3> createDomesticStandingOrders(
+            @Valid OBWriteDomesticStandingOrder3 OBWriteDomesticStandingOrder3,
+            String xFapiFinancialId,
+            String authorization,
+            String xIdempotencyKey,
+            String xJwsSignature,
+            String xAccountId,
+            DateTime xFapiCustomerLastLoggedTime,
+            String xFapiCustomerIpAddress,
+            String xFapiInteractionId,
+            String xCustomerUserAgent,
+            HttpServletRequest request,
+            Principal principal) throws OBErrorResponseException {
+        log.debug("Received payment submission: '{}'", OBWriteDomesticStandingOrder3);
+
+        paymentSubmissionValidator.validateIdempotencyKeyAndRisk(xIdempotencyKey, OBWriteDomesticStandingOrder3.getRisk());
+
+        FRWriteDomesticStandingOrder frStandingOrder = toFRWriteDomesticStandingOrder(OBWriteDomesticStandingOrder3);
+        log.trace("Converted to: '{}'", frStandingOrder);
+
+        FRDomesticStandingOrderPaymentSubmission frPaymentSubmission = FRDomesticStandingOrderPaymentSubmission.builder()
+                .id(UUID.randomUUID().toString())
+                .standingOrder(frStandingOrder)
+                .status(INITIATIONPENDING)
+                .created(new DateTime())
+                .updated(new DateTime())
+                .idempotencyKey(xIdempotencyKey)
+                .obVersion(VersionPathExtractor.getVersionFromPath(request))
+                .build();
+
+        // Save the standing order
+        frPaymentSubmission = new IdempotentRepositoryAdapter<>(standingOrderPaymentSubmissionRepository)
+                .idempotentSave(frPaymentSubmission);
+
+        // Save the standing order data for the Accounts API
+        FRStandingOrderData standingOrderData = createFRStandingOrderData(frStandingOrder.getData().getInitiation(), xAccountId);
+        standingOrderService.createStandingOrder(standingOrderData);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseEntity(frPaymentSubmission));
+    }
+
+    @Override
+    public ResponseEntity getDomesticStandingOrdersDomesticStandingOrderId(
+            String domesticStandingOrderId,
+            String xFapiFinancialId,
+            String authorization,
+            DateTime xFapiCustomerLastLoggedTime,
+            String xFapiCustomerIpAddress,
+            String xFapiInteractionId,
+            String xCustomerUserAgent,
+            HttpServletRequest request,
+            Principal principal) throws OBErrorResponseException {
+        Optional<FRDomesticStandingOrderPaymentSubmission> isPaymentSubmission = standingOrderPaymentSubmissionRepository.findById(domesticStandingOrderId);
+        if (!isPaymentSubmission.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Payment submission '" + domesticStandingOrderId + "' can't be found");
+        }
+
+        return ResponseEntity.ok(responseEntity(isPaymentSubmission.get()));
+    }
+
+    private OBWriteDomesticStandingOrderResponse3 responseEntity(FRDomesticStandingOrderPaymentSubmission frPaymentSubmission) {
+        return new OBWriteDomesticStandingOrderResponse3().data(new OBWriteDataDomesticStandingOrderResponse3()
+                .domesticStandingOrderId(frPaymentSubmission.getId())
+                .initiation(toOBDomesticStandingOrder3(frPaymentSubmission.getStandingOrder().getData().getInitiation()))
+                .creationDateTime(frPaymentSubmission.getCreated())
+                .statusUpdateDateTime(frPaymentSubmission.getUpdated())
+                .status(toOBExternalStatus1Code(frPaymentSubmission.getStatus()))
+                .consentId(frPaymentSubmission.getStandingOrder().getData().getConsentId()))
+                .links(createDomesticStandingOrderPaymentLink(this.getClass(), frPaymentSubmission.getId()))
+                .meta(new Meta());
+    }
+
+}
---
+++ securebanking-openbanking-uk-rs-simulator/src/main/java/com/forgerock/securebanking/openbanking/uk/rs/api/obie/payment/v3_1_1/internationalstandingorders/InternationalStandingOrdersApiController.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright © 2020 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.securebanking.openbanking.uk.rs.api.obie.payment.v3_1_1.internationalstandingorders;
+
+import com.forgerock.securebanking.openbanking.uk.error.OBErrorResponseException;
+import com.forgerock.securebanking.openbanking.uk.rs.persistence.document.payment.FRInternationalStandingOrderPaymentSubmission;
+import com.forgerock.securebanking.openbanking.uk.rs.persistence.repository.payments.InternationalStandingOrderPaymentSubmissionRepository;
+import com.forgerock.securebanking.openbanking.uk.rs.service.standingorder.StandingOrderService;
+import com.forgerock.securebanking.openbanking.uk.rs.validator.PaymentSubmissionValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import uk.org.openbanking.datamodel.account.Meta;
+import uk.org.openbanking.datamodel.payment.OBWriteDataInternationalStandingOrderResponse3;
+import uk.org.openbanking.datamodel.payment.OBWriteInternationalStandingOrder3;
+import uk.org.openbanking.datamodel.payment.OBWriteInternationalStandingOrderResponse3;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.security.Principal;
+
+import static com.forgerock.securebanking.openbanking.uk.rs.api.obie.payment.LinksHelper.createInternationalStandingOrderPaymentLink;
+import static com.forgerock.securebanking.openbanking.uk.rs.converter.payment.FRSubmissionStatusConverter.toOBExternalStatus1Code;
+import static com.forgerock.securebanking.openbanking.uk.rs.converter.payment.FRWriteInternationalStandingOrderConsentConverter.toOBInternationalStandingOrder3;
+
+@Controller("InternationalStandingOrdersApiV3.1.1")
+@Slf4j
+public class InternationalStandingOrdersApiController implements InternationalStandingOrdersApi {
+
+    private final InternationalStandingOrderPaymentSubmissionRepository standingOrderPaymentSubmissionRepository;
+    private final PaymentSubmissionValidator paymentSubmissionValidator;
+    private final StandingOrderService standingOrderService;
+
+    public InternationalStandingOrdersApiController(
+            InternationalStandingOrderPaymentSubmissionRepository standingOrderPaymentSubmissionRepository,
+            PaymentSubmissionValidator paymentSubmissionValidator,
+            StandingOrderService standingOrderService) {
+        this.standingOrderPaymentSubmissionRepository = standingOrderPaymentSubmissionRepository;
+        this.paymentSubmissionValidator = paymentSubmissionValidator;
+        this.standingOrderService = standingOrderService;
+    }
+
+    @Override
+    public ResponseEntity<OBWriteInternationalStandingOrderResponse3> createInternationalStandingOrders(
+            @Valid OBWriteInternationalStandingOrder3 OBWriteInternationalStandingOrder3,
+            String xFapiFinancialId,
+            String authorization,
+            String xIdempotencyKey,
+            String xJwsSignature,
+            String xAccountId,
+            DateTime xFapiCustomerLastLoggedTime,
+            String xFapiCustomerIpAddress,
+            String xFapiInteractionId,
+            String xCustomerUserAgent,
+            HttpServletRequest request,
+            Principal principal) throws OBErrorResponseException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<OBWriteInternationalStandingOrderResponse3> getInternationalStandingOrdersInternationalStandingOrderPaymentId(
+            String internationalStandingOrderPaymentId,
+            String xFapiFinancialId,
+            String authorization,
+            DateTime xFapiCustomerLastLoggedTime,
+            String xFapiCustomerIpAddress,
+            String xFapiInteractionId,
+            String xCustomerUserAgent,
+            HttpServletRequest request,
+            Principal principal) throws OBErrorResponseException {
+        return null;
+    }
+
+    private OBWriteInternationalStandingOrderResponse3 responseEntity(FRInternationalStandingOrderPaymentSubmission frPaymentSubmission) {
+        return new OBWriteInternationalStandingOrderResponse3()
+                .data(new OBWriteDataInternationalStandingOrderResponse3()
+                        .internationalStandingOrderId(frPaymentSubmission.getId())
+                        .initiation(toOBInternationalStandingOrder3(frPaymentSubmission.getStandingOrder().getData().getInitiation()))
+                        .creationDateTime(frPaymentSubmission.getCreated())
+                        .statusUpdateDateTime(frPaymentSubmission.getUpdated())
+                        .status(toOBExternalStatus1Code(frPaymentSubmission.getStatus()))
+                        .consentId(frPaymentSubmission.getStandingOrder().getData().getConsentId())
+                )
+                .links(createInternationalStandingOrderPaymentLink(this.getClass(), frPaymentSubmission.getId()))
+                .meta(new Meta());
+    }
+}
