15cafeeaa2a8c84c344f7d96af65d83ab5d67f77
Andra <andra.racovita@itsmartsystems.eu>
2022-06-22 17:56:00 +0300

    341: Change the import path for FRFrequencyType and FRQuarterType
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/341

--- securebanking-openbanking-uk-rs-simulator-server/src/main/java/com/forgerock/securebanking/openbanking/uk/rs/service/frequency/FrequencyService.java
+++ securebanking-openbanking-uk-rs-simulator-server/src/main/java/com/forgerock/securebanking/openbanking/uk/rs/service/frequency/FrequencyService.java
@@ -15,8 +15,8 @@
  */
 package com.forgerock.securebanking.openbanking.uk.rs.service.frequency;
 
-import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.payment.FRFrequencyType;
-import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.payment.FRQuarterType;
+import com.forgerock.securebanking.openbanking.uk.common.api.meta.forgerock.FRFrequencyType;
+import com.forgerock.securebanking.openbanking.uk.common.api.meta.forgerock.FRQuarterType;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 
