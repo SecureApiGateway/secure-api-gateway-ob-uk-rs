20f9465f55e873057d9a58191b6dfb8d8df58d17
frlaura-jianu <laura.jianu@forgerock.com>
2023-01-11 11:07:00 +0200

    Adding validations for creditorAccount & MaximumIndividualAmount

--- securebanking-openbanking-uk-rs-simulator-server/src/main/java/com/forgerock/securebanking/openbanking/uk/rs/api/backoffice/payment/validation/services/DomesticVrpValidationService.java
+++ securebanking-openbanking-uk-rs-simulator-server/src/main/java/com/forgerock/securebanking/openbanking/uk/rs/api/backoffice/payment/validation/services/DomesticVrpValidationService.java
@@ -16,14 +16,19 @@
 package com.forgerock.securebanking.openbanking.uk.rs.api.backoffice.payment.validation.services;
 
 import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.vrp.FRDomesticVrpRequest;
+import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.vrp.FRDomesticVrpRequestData;
 import com.forgerock.securebanking.common.openbanking.uk.forgerock.datamodel.vrp.FRWriteDomesticVrpDataInitiation;
+import com.forgerock.securebanking.openbanking.uk.error.OBErrorResponseException;
 import com.forgerock.securebanking.openbanking.uk.rs.validator.OBRisk1Validator;
 import com.forgerock.securebanking.openbanking.uk.error.OBErrorException;
 import com.forgerock.securebanking.openbanking.uk.error.OBRIErrorType;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 import uk.org.openbanking.datamodel.common.OBRisk1;
 import uk.org.openbanking.datamodel.error.OBError1;
+import uk.org.openbanking.datamodel.error.OBStandardErrorCodes1;
+import uk.org.openbanking.datamodel.error.StandardErrorCode;
 import uk.org.openbanking.datamodel.vrp.*;
 
 
@@ -40,6 +45,7 @@ import static com.forgerock.securebanking.common.openbanking.uk.forgerock.datamo
 public class DomesticVrpValidationService {
 
     protected List<OBError1> errors = new ArrayList<>();
+    private OBDomesticVRPConsentRequest request;
 
     public DomesticVrpValidationService clearErrors() {
         this.errors.clear();
@@ -52,15 +58,15 @@ public class DomesticVrpValidationService {
 
     private OBRisk1Validator riskValidator;
 
-    public void validate(OBDomesticVRPInitiation initiation, OBDomesticVRPInstruction instruction, OBRisk1 risk, FRDomesticVrpRequest frDomesticVRPRequest) throws OBErrorException {
+    public void validate(OBDomesticVRPInitiation initiation, OBDomesticVRPInstruction instruction, OBRisk1 risk, FRDomesticVrpRequest frDomesticVRPRequest, OBDomesticVRPRequest requestCreditorAccount) throws OBErrorException {
         this.riskValidator = riskValidator;
 
         checkRequestAndConsentInitiationMatch(initiation, frDomesticVRPRequest);
         checkRequestAndConsentRiskMatch(risk, frDomesticVRPRequest);
         validateRisk(risk);
-        checkControlParameters(instruction, frDomesticVRPRequest);
+        checkCreditorAccountPresentInInstructionIfNotPresentInConsent(requestCreditorAccount, frDomesticVRPRequest);
+        //checkControlParameters(instruction, frDomesticVRPRequest);
         // TODO - implement check on creditor account
-        //checkCreditorAccountExistence();
     }
 
     //initiation - request vs consent
@@ -93,38 +99,57 @@ public class DomesticVrpValidationService {
         }
     }
 
-    //controlParameters - validation
-    public void checkControlParameters(OBDomesticVRPInstruction instruction, FRDomesticVrpRequest consent) throws OBErrorException {
-        validateMaximumIndividualAmount(instruction, consent);
+    //if the CreditorAccount was not specified in the the consent, the CreditorAccount must be specified in the instruction
+    public void checkCreditorAccountPresentInInstructionIfNotPresentInConsent(OBDomesticVRPRequest requestCreditorAccount, FRDomesticVrpRequest consent) throws OBErrorException {
+        if (consent.getData().getInitiation().getCreditorAccount() == null) {
+            if (requestCreditorAccount.getData().getInstruction().getCreditorAccount() == null) {
+                throw new OBErrorException(OBRIErrorType.REQUEST_VRP_CREDITOR_ACCOUNT_NOT_SPECIFIED);
+            }
+        }
     }
+
+    //controlParameters - validation
+    public void checkControlParameters(OBDomesticVRPControlParameters controlParameters, OBDomesticVRPInstruction instruction, FRDomesticVrpRequest consent) throws OBErrorException {
+        validatePsuAuthenticationMethods(controlParameters.getPsUAuthenticationMethods());
+        validatePsuInteractionTypes(controlParameters.getPsUInteractionTypes());
+        validateVRPType(controlParameters.getVrPType());
+        validateFromToDateTimes(controlParameters.getValidFromDateTime(), controlParameters.getValidToDateTime());
+        validateMaximumIndividualAmount(instruction, consent);
+        validatePeriodicLimits(controlParameters.getPeriodicLimits());
+    }
+
+    private void validatePeriodicLimits(List<OBDomesticVRPControlParametersPeriodicLimits> periodicLimits) {
+    }
+
+    private void validateFromToDateTimes(DateTime validFromDateTime, DateTime validToDateTime) {
+    }
+
+    private void validateVRPType(List<String> vrPType) {
+    }
+
+    private void validatePsuInteractionTypes(List<OBVRPInteractionTypes> psUInteractionTypes) {
+    }
+
+    private void validatePsuAuthenticationMethods(List<String> psUAuthenticationMethods) throws OBErrorException {
+    }
+
 
     private void validateMaximumIndividualAmount(OBDomesticVRPInstruction instruction, FRDomesticVrpRequest consent) throws OBErrorException {
         String instructionAmount = String.valueOf(instruction.getInstructedAmount().getAmount());
         String instructionCurrency = String.valueOf(instruction.getInstructedAmount().getCurrency());
         // TODO - next method must be implemented correctly depending on the control parameters rules set for the payment periodic limits and maximum
         // individual ammount
-        validateMaximumIndividualAmount(consent, Double.valueOf(instructionAmount), Double.valueOf(instructionCurrency));
+        validateMaximumIndividualAmount(request, consent, Double.valueOf(instructionAmount), Double.valueOf(instructionCurrency));
     }
 
-    private void validateMaximumIndividualAmount(FRDomesticVrpRequest consent, Double instructionAmount, Double instructionCurrency) {
-        /*get controlParameters
-        String consentAmount = String.valueOf(consent.controlParameters.getMaximumIndividualAmount().getAmount());
-        String currencyAmount = String.valueOf(consent.controlParameters.getMaximumIndividualAmount().getCurrency());
-        if (instructionAmount.compareTo(consentAmount)) {
+    //MaximumIndividualAmount - validation
+    private void validateMaximumIndividualAmount(OBDomesticVRPConsentRequest request, FRDomesticVrpRequest consent, Double instructionAmount, Double instructionCurrency) throws OBErrorException {
+        OBDomesticVRPControlParameters controlParameters = request.getData().getControlParameters();
+        Double consentAmount = Double.valueOf(controlParameters.getMaximumIndividualAmount().getAmount());
+        Double consentCurrency = Double.valueOf(controlParameters.getMaximumIndividualAmount().getCurrency());
+        if ((instructionAmount.compareTo(consentAmount) > 0) && (instructionCurrency.compareTo(consentCurrency) > 0)) {
             throw new OBErrorException(
-                    OBRIErrorType.REQUEST_VRP_CONTROL_PARAMETERS_RULES,
-                    pass in the control parameter field that caused the error in the Field field of the error message;
-        );
-        }
-    }*/
-    }
-
-    //if the CreditorAccount was not specified in the the consent, the CreditorAccount must be specified in the instruction
-    public void checkCreditorAccountExistence(OBDomesticVRPRequest request, FRDomesticVrpRequest consent) throws OBErrorException {
-        if (consent.getData().getInitiation().getCreditorAccount() == null) {
-            if (request.getData().getInstruction().getCreditorAccount() == null) {
-                throw new OBErrorException(OBRIErrorType.REQUEST_VRP_CREDITOR_ACCOUNT_NOT_SPECIFIED);
-            }
+                    OBRIErrorType.REQUEST_VRP_CONTROL_PARAMETERS_RULES);
         }
     }
 }
