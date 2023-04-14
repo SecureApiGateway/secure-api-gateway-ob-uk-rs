080b43b9f460f12163b16cb760c24d6e3d09f118
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2023-04-14 11:39:30 +0100

    Logging improvement when risk is null

--- secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/services/validation/RiskValidationService.java
+++ secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/services/validation/RiskValidationService.java
@@ -46,8 +46,7 @@ public class RiskValidationService {
      */
     public void validate(OBRisk1 consentRisk, OBRisk1 requestRisk) throws OBErrorException {
         if (Objects.isNull(requestRisk)) {
-            log.error("Request Risk cannot be null");
-            throw new OBErrorException(OBRIErrorType.PAYMENT_INVALID_RISK, "Request Risk cannot be null");
+            throwError("The Risk property cannot be null");
         }
         checkEquality(consentRisk.getBeneficiaryAccountType(), requestRisk.getBeneficiaryAccountType());
         checkEquality(
