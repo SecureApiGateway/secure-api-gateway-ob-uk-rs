ec0f70e2c9215b78026b532b8b3828e3d62b779a
Dan Badham <dan.badham@forgerock.com>
2023-06-15 12:14:05 +0100

    1012: Adding OBWriteDomesticConsentResponse5FactoryTest
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1012

--- secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/factories/OBWriteDomesticConsentResponse5Factory.java
+++ secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/factories/OBWriteDomesticConsentResponse5Factory.java
@@ -15,6 +15,8 @@
  */
 package com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.payment.factories;
 
+import static com.forgerock.sapi.gateway.ob.uk.rs.server.common.util.link.LinksHelper.createDomesticPaymentConsentsLink;
+
 import org.springframework.stereotype.Component;
 
 import com.forgerock.sapi.gateway.ob.uk.rs.server.common.util.link.LinksHelper;
@@ -26,29 +28,30 @@ import uk.org.openbanking.datamodel.payment.OBWriteDomesticConsentResponse5;
 import uk.org.openbanking.datamodel.payment.OBWriteDomesticConsentResponse5Data;
 import uk.org.openbanking.datamodel.payment.OBWriteDomesticConsentResponse5Data.StatusEnum;
 
+/**
+ * Factory to create {@link OBWriteDomesticConsentResponse5}
+ *
+ * The response is built from a {@link DomesticPaymentConsent}
+ */
 @Component
 public class OBWriteDomesticConsentResponse5Factory {
 
     public OBWriteDomesticConsentResponse5 buildConsentResponse(DomesticPaymentConsent domesticPaymentConsent, Class<?> controllerClass) {
-        final OBWriteDomesticConsentResponse5 consentResponse = new OBWriteDomesticConsentResponse5();
         final OBWriteDomesticConsentResponse5Data data = new OBWriteDomesticConsentResponse5Data();
         final OBWriteDomesticConsent4Data consentRequestData = domesticPaymentConsent.getRequestObj().getData();
         data.authorisation(consentRequestData.getAuthorisation());
         data.readRefundAccount(consentRequestData.getReadRefundAccount());
         data.scASupportData(consentRequestData.getScASupportData());
         data.initiation(consentRequestData.getInitiation());
-
         data.charges(domesticPaymentConsent.getCharges());
         data.consentId(domesticPaymentConsent.getId());
         data.status(StatusEnum.fromValue(domesticPaymentConsent.getStatus()));
         data.creationDateTime(domesticPaymentConsent.getCreationDateTime());
         data.statusUpdateDateTime(domesticPaymentConsent.getStatusUpdateDateTime());
 
-        consentResponse.setData(data);
-
-        consentResponse.setRisk(domesticPaymentConsent.getRequestObj().getRisk());
-        consentResponse.links(LinksHelper.createDomesticPaymentConsentsLink(controllerClass, domesticPaymentConsent.getId())).meta(new Meta());
-
-        return consentResponse;
+        return new OBWriteDomesticConsentResponse5().data(data)
+                                                    .risk(domesticPaymentConsent.getRequestObj().getRisk())
+                                                    .links(createDomesticPaymentConsentsLink(controllerClass, domesticPaymentConsent.getId()))
+                                                    .meta(new Meta());
     }
 }
