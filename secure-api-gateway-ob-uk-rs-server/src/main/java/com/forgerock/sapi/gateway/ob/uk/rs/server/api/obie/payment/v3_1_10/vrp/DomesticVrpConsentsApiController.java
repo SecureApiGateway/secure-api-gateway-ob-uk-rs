e63c69517c19c1e46ca51f3caec6666820817917
Laura Jianu <108405474+frlaura-jianu@users.noreply.github.com>
2023-06-27 17:46:50 +0300

    984: Adding domestic vrp funds confirmation RS logic (#192)
    * 984: Adding DomesticVrpConsentsApi and Controller
    
    We need the DomesticVrpConsentsApi (and Controller) in order to support the `funds-confirmation` API Operation. Other payments APIs reuse a backoffice controller for this purpose, however VRP request is more complex and it makes sense to have its own controller.
    
    As we do not currently support Consent APIs in this component, we have removed the other API operations from the generated code; only the funds confirmations functionality is available.
    
    The DomesticVrpConsentsApiController is just a generated stub at the moment (which throws UnsupportedOperationException), real implementation to be provided.
    
    DomesticVrpConsentsApiControllerTest integration test which can be used to call the API.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/984
    
    * 984: Adding copyright
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/984
    
    * 984: Adding HTTP POST domestic vrp funds confirmation support
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/984
    Description:
    - Switching the HTTP operation from GET to POST for the domestic vrp funds confirmation endpoint
    
    * 984: implementation vrp funds confirmation
    - Added the implementation to support vrp funds confirmation
    - Added test coverage
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/984
    
    * Test improvements
    
    * added logs
    
    * fix tests mandatory data delegated to spring bean validation
    
    * Fix AccountId json field to lowercase accountId
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/984
    
    ---------
    
    Co-authored-by: Dan Badham <dan.badham@forgerock.com>
    Co-authored-by: jorgesanchezperez <jorge.sanchez-perez@forgerock.com>

---
+++ secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/v3_1_10/vrp/DomesticVrpConsentsApiController.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.payment.v3_1_10.vrp;
+
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBErrorResponseException;
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBRIErrorResponseCategory;
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBRIErrorType;
+import com.forgerock.sapi.gateway.ob.uk.rs.obie.api.payment.v3_1_10.vrp.DomesticVrpConsentsApi;
+import com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.payment.services.ConsentService;
+import com.forgerock.sapi.gateway.ob.uk.rs.server.service.balance.FundsAvailabilityService;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import uk.org.openbanking.datamodel.vrp.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+@Controller("DomesticVrpConsentApiV3.1.10")
+@Slf4j
+public class DomesticVrpConsentsApiController implements DomesticVrpConsentsApi {
+
+    private final FundsAvailabilityService fundsAvailabilityService;
+    private final ConsentService consentService;
+
+    public DomesticVrpConsentsApiController(FundsAvailabilityService fundsAvailabilityService, ConsentService consentService) {
+        this.fundsAvailabilityService = fundsAvailabilityService;
+        this.consentService = consentService;
+    }
+
+    @Override
+    public ResponseEntity<OBVRPFundsConfirmationResponse> domesticVrpConsentsFundsConfirmation(String consentId,
+                                                                                               String authorization,
+                                                                                               String xJwsSignature,
+                                                                                               OBVRPFundsConfirmationRequest obVRPFundsConfirmationRequest,
+                                                                                               String xFapiAuthDate,
+                                                                                               String xFapiCustomerIpAddress,
+                                                                                               String xFapiInteractionId,
+                                                                                               String xCustomerUserAgent,
+                                                                                               HttpServletRequest request
+    ) throws OBErrorResponseException {
+        validateRequest(obVRPFundsConfirmationRequest, consentId);
+
+        JsonObject intent = consentService.getIDMIntent(authorization, consentId);
+        log.debug("Retrieved consent from IDM");
+        String accountId = intent.get("accountId").getAsString();
+
+        String amount = obVRPFundsConfirmationRequest.getData().getInstructedAmount().getAmount();
+
+        // Check if funds are available on the account
+        boolean areFundsAvailable = fundsAvailabilityService.isFundsAvailable(accountId, amount);
+        log.debug("Are funds available {}", areFundsAvailable);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        new OBVRPFundsConfirmationResponse()
+                                .data(
+                                        new OBVRPFundsConfirmationResponseData()
+                                                .consentId(obVRPFundsConfirmationRequest.getData().getConsentId())
+                                                .reference(obVRPFundsConfirmationRequest.getData().getReference())
+                                                .fundsConfirmationId(UUID.randomUUID().toString())
+                                                .fundsAvailableResult(
+                                                        new OBPAFundsAvailableResult1()
+                                                                .fundsAvailable(
+                                                                        areFundsAvailable ?
+                                                                                OBPAFundsAvailableResult1.FundsAvailableEnum.AVAILABLE :
+                                                                                OBPAFundsAvailableResult1.FundsAvailableEnum.NOTAVAILABLE
+                                                                )
+                                                                .fundsAvailableDateTime(DateTime.now())
+                                                )
+                                                .instructedAmount(
+                                                        obVRPFundsConfirmationRequest.getData().getInstructedAmount()
+                                                )
+                                )
+                );
+    }
+
+    private void validateRequest(
+            OBVRPFundsConfirmationRequest obVRPFundsConfirmationRequest,
+            String consentId
+    ) throws OBErrorResponseException {
+        log.debug("Validating request {}", obVRPFundsConfirmationRequest.toString());
+        OBVRPFundsConfirmationRequestData data = obVRPFundsConfirmationRequest.getData();
+        if (Objects.isNull(data) || Objects.isNull(data.getInstructedAmount()) || Objects.isNull(data.getInstructedAmount().getAmount())) {
+            String reason = "Mandatory data not provided.";
+            log.error(reason);
+            throw new OBErrorResponseException(
+                    BAD_REQUEST,
+                    OBRIErrorResponseCategory.REQUEST_INVALID,
+                    OBRIErrorType.REQUEST_OBJECT_INVALID.toOBError1(reason)
+            );
+        }
+        if (!obVRPFundsConfirmationRequest.getData().getConsentId().equals(consentId)) {
+            String reason = "The consentId provided in the body doesn't match with the consent id provided as parameter";
+            log.error(reason);
+            throw new OBErrorResponseException(
+                    BAD_REQUEST,
+                    OBRIErrorResponseCategory.REQUEST_INVALID,
+                    OBRIErrorType.REQUEST_OBJECT_INVALID.toOBError1(
+                            reason
+                    )
+            );
+        }
+    }
+}
