4b9e181c9b6160192f0454eae9433304c8113bcf
Dan Badham <dan.badham@forgerock.com>
2023-07-26 15:11:55 +0100

    1064: Adding validators for International Standing Orders
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1064

---
+++ secure-api-gateway-ob-uk-rs-validation/secure-api-gateway-ob-uk-rs-validation-obie/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/validation/obie/payment/consent/OBWriteInternationalStandingOrderConsent6Validator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.ob.uk.rs.validation.obie.payment.consent;
+
+import java.util.Objects;
+
+import com.forgerock.sapi.gateway.ob.uk.rs.validation.ValidationResult;
+import com.forgerock.sapi.gateway.ob.uk.rs.validation.obie.BaseOBValidator;
+
+import uk.org.openbanking.datamodel.error.OBError1;
+import uk.org.openbanking.datamodel.payment.OBWriteDomestic2DataInitiationInstructedAmount;
+import uk.org.openbanking.datamodel.payment.OBWriteInternationalStandingOrder4DataInitiation;
+import uk.org.openbanking.datamodel.payment.OBWriteInternationalStandingOrderConsent6;
+
+/**
+ * Validator of OBWriteInternationalStandingOrderConsent6V objects (OBIE International Standing Order Consents)
+ */
+public class OBWriteInternationalStandingOrderConsent6Validator extends BaseOBValidator<OBWriteInternationalStandingOrderConsent6> {
+
+    private final BaseOBValidator<OBWriteDomestic2DataInitiationInstructedAmount> instructedAmountValidator;
+    private final BaseOBValidator<String> currencyCodeValidator;
+
+    public OBWriteInternationalStandingOrderConsent6Validator(BaseOBValidator<OBWriteDomestic2DataInitiationInstructedAmount> instructedAmountValidator,
+                                                              BaseOBValidator<String> currencyCodeValidator) {
+        this.instructedAmountValidator = Objects.requireNonNull(instructedAmountValidator, "instructedAmountValidator must be supplied");
+        this.currencyCodeValidator = Objects.requireNonNull(currencyCodeValidator, "currencyCodeValidator must be supplied");
+    }
+
+    @Override
+    protected void validate(OBWriteInternationalStandingOrderConsent6 obj, ValidationResult<OBError1> validationResult) {
+        final OBWriteInternationalStandingOrder4DataInitiation initiation = obj.getData().getInitiation();
+        validationResult.mergeResults(currencyCodeValidator.validate(initiation.getCurrencyOfTransfer()));
+        validationResult.mergeResults(instructedAmountValidator.validate(initiation.getInstructedAmount()));
+    }
+}
