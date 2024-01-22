7cb0f095aab961dc1e8bb25cf7b34e6cee232c22
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2024-01-22 15:06:55 +0000

    1088: Removing all OB Events API versions older than v3.1.10 (#278)
    Older API versions are not supported by the Consent Store, and are so old that not ASPSP should be offering them.
    
    Where v3.1.10 controllers are extending older version controllers, the logic from the version with the impl being used has been pulled up into the v3.1.10 controller.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1088

--- secure-api-gateway-ob-uk-rs-obie-api/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/obie/api/event/v3_1_10/eventsubscription/EventSubscriptionsApi.java
+++ secure-api-gateway-ob-uk-rs-obie-api/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/obie/api/event/v3_1_10/eventsubscription/EventSubscriptionsApi.java
@@ -15,11 +15,200 @@
  */
 package com.forgerock.sapi.gateway.ob.uk.rs.obie.api.event.v3_1_10.eventsubscription;
 
+import java.security.Principal;
+
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBErrorResponseException;
 import com.forgerock.sapi.gateway.ob.uk.rs.obie.api.swagger.SwaggerApiTags;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.AuthorizationScope;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import uk.org.openbanking.datamodel.error.OBErrorResponse1;
+import uk.org.openbanking.datamodel.event.OBEventSubscription1;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionResponse1;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionsResponse1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Api(tags = {"v3.1.10", SwaggerApiTags.EVENT_NOTIFICATION_TAG})
-@RequestMapping(value = "/open-banking/v3.1.10/event-subscriptions")
-public interface EventSubscriptionsApi extends com.forgerock.sapi.gateway.ob.uk.rs.obie.api.event.v3_1_9.eventsubscription.EventSubscriptionsApi {
+@RequestMapping(value = "/open-banking/v3.1.10")
+public interface EventSubscriptionsApi {
+
+    String BASE_PATH = "/event-subscriptions";
+
+    @ApiOperation(value = "Create an event subscription", nickname = "createEventSubscription", notes = "", response = OBEventSubscriptionResponse1.class, authorizations = {
+            @Authorization(value = "TPPOAuth2Security", scopes = {
+                    @AuthorizationScope(scope = "accounts", description = "Accounts scope"),
+                    @AuthorizationScope(scope = "payments", description = "Payments  scope"),
+                    @AuthorizationScope(scope = "fundsconfirmations", description = "Funds Confirmations scope")
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Event subscriptions Created", response = OBEventSubscriptionResponse1.class),
+            @ApiResponse(code = 400, message = "Bad request", response = OBErrorResponse1.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 405, message = "Method Not Allowed"),
+            @ApiResponse(code = 406, message = "Not Acceptable"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 415, message = "Unsupported Media Type"),
+            @ApiResponse(code = 429, message = "Too Many Requests"),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = OBErrorResponse1.class)})
+
+    @RequestMapping(value = BASE_PATH,
+            produces = {"application/json; charset=utf-8"},
+            consumes = {"application/json; charset=utf-8"},
+            method = RequestMethod.POST)
+    ResponseEntity<OBEventSubscriptionResponse1> createEventSubscription(
+            @ApiParam(value = "Default", required = true)
+            @Valid
+            @RequestBody OBEventSubscription1 obEventSubscription,
+
+            @ApiParam(value = "An Authorisation Token as per https://tools.ietf.org/html/rfc6750", required = true)
+            @RequestHeader(value = "Authorization", required = true) String authorization,
+
+            @ApiParam(value = "Header containing a detached JWS signature of the body of the payload.", required = true)
+            @RequestHeader(value = "x-jws-signature", required = false) String xJwsSignature,
+
+            @ApiParam(value = "An RFC4122 UID used as a correlation id.")
+            @RequestHeader(value = "x-fapi-interaction-id", required = false) String xFapiInteractionId,
+
+            @ApiParam(value = "The PISP TPP ID (otherwise referred to as the Client ID")
+            @RequestHeader(value = "x-ob-tpp-id", required = true) String tppId,
+
+            HttpServletRequest request,
+
+            Principal principal
+    ) throws OBErrorResponseException;
+
+    @ApiOperation(value = "Read Event Subscription", nickname = "read", notes = "", response = OBEventSubscriptionsResponse1.class, authorizations = {
+            @Authorization(value = "TPPOAuth2Security", scopes = {
+                    @AuthorizationScope(scope = "accounts", description = "Accounts scope"),
+                    @AuthorizationScope(scope = "payments", description = "Payments  scope"),
+                    @AuthorizationScope(scope = "fundsconfirmations", description = "Funds Confirmations scope")
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Event Subscriptiopn Read", response = OBEventSubscriptionsResponse1.class),
+            @ApiResponse(code = 400, message = "Bad request", response = OBErrorResponse1.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 405, message = "Method Not Allowed"),
+            @ApiResponse(code = 406, message = "Not Acceptable"),
+            @ApiResponse(code = 429, message = "Too Many Requests"),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = OBErrorResponse1.class)})
+
+    @RequestMapping(value = BASE_PATH,
+            produces = {"application/json; charset=utf-8"},
+            method = RequestMethod.GET)
+    ResponseEntity<OBEventSubscriptionsResponse1> readEventSubscription(
+            @ApiParam(value = "An Authorisation Token as per https://tools.ietf.org/html/rfc6750", required = true)
+            @RequestHeader(value = "Authorization", required = true) String authorization,
+
+            @ApiParam(value = "An RFC4122 UID used as a correlation id.")
+            @RequestHeader(value = "x-fapi-interaction-id", required = false) String xFapiInteractionId,
+
+            @ApiParam(value = "The PISP TPP ID (otherwise referred to as the Client ID")
+            @RequestHeader(value = "x-ob-tpp-id", required = true) String tppId,
+
+            HttpServletRequest request,
+
+            Principal principal
+    ) throws OBErrorResponseException;
+
+    @ApiOperation(value = "Amend an event subscription", nickname = "amend", notes = "", response = OBEventSubscriptionsResponse1.class, authorizations = {
+            @Authorization(value = "TPPOAuth2Security", scopes = {
+                    @AuthorizationScope(scope = "accounts", description = "Accounts scope"),
+                    @AuthorizationScope(scope = "payments", description = "Payments  scope"),
+                    @AuthorizationScope(scope = "fundsconfirmations", description = "Funds Confirmations scope")
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Event Subscription Amended", response = OBEventSubscriptionResponse1.class),
+            @ApiResponse(code = 400, message = "Bad request", response = OBErrorResponse1.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 405, message = "Method Not Allowed"),
+            @ApiResponse(code = 406, message = "Not Acceptable"),
+            @ApiResponse(code = 429, message = "Too Many Requests"),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = OBErrorResponse1.class)})
+
+    @RequestMapping(value = "/event-subscriptions/{EventSubscriptionId}",
+            produces = {"application/json; charset=utf-8"},
+            consumes = {"application/json; charset=utf-8"},
+            method = RequestMethod.PUT)
+    ResponseEntity<OBEventSubscriptionResponse1> updateEventSubscription(
+            @ApiParam(value = "EventSubscriptionId", required = true)
+            @PathVariable("EventSubscriptionId") String eventSubscriptionId,
+
+            @ApiParam(value = "Default", required = true)
+            @Valid
+            @RequestBody OBEventSubscriptionResponse1 obEventSubscription,
+
+            @ApiParam(value = "An Authorisation Token as per https://tools.ietf.org/html/rfc6750", required = true)
+            @RequestHeader(value = "Authorization", required = true) String authorization,
+
+            @ApiParam(value = "Header containing a detached JWS signature of the body of the payload.", required = true)
+            @RequestHeader(value = "x-jws-signature", required = false) String xJwsSignature,
+
+            @ApiParam(value = "An RFC4122 UID used as a correlation id.")
+            @RequestHeader(value = "x-fapi-interaction-id", required = false) String xFapiInteractionId,
+
+            @ApiParam(value = "The PISP TPP ID (otherwise referred to as the Client ID")
+            @RequestHeader(value = "x-ob-tpp-id", required = true) String tppId,
+
+            HttpServletRequest request,
+
+            Principal principal
+    ) throws OBErrorResponseException;
+
+    @ApiOperation(value = "Delete an event subscription", nickname = "delete", notes = "", authorizations = {
+            @Authorization(value = "TPPOAuth2Security", scopes = {
+                    @AuthorizationScope(scope = "accounts", description = "Accounts scope"),
+                    @AuthorizationScope(scope = "payments", description = "Payments  scope"),
+                    @AuthorizationScope(scope = "fundsconfirmations", description = "Funds Confirmations scope")
+            })
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Event Subscription Deleted", response = Void.class),
+            @ApiResponse(code = 400, message = "Bad request", response = OBErrorResponse1.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 405, message = "Method Not Allowed"),
+            @ApiResponse(code = 406, message = "Not Acceptable"),
+            @ApiResponse(code = 429, message = "Too Many Requests"),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = OBErrorResponse1.class)})
+
+    @RequestMapping(value = "/event-subscriptions/{EventSubscriptionId}",
+            method = RequestMethod.DELETE)
+    ResponseEntity deleteEventSubscription(
+            @ApiParam(value = "EventSubscriptionId", required = true)
+            @PathVariable("EventSubscriptionId") String eventSubscriptionId,
+
+            @ApiParam(value = "An Authorisation Token as per https://tools.ietf.org/html/rfc6750", required = true)
+            @RequestHeader(value = "Authorization", required = true) String authorization,
+
+            @ApiParam(value = "An RFC4122 UID used as a correlation id.")
+            @RequestHeader(value = "x-fapi-interaction-id", required = false) String xFapiInteractionId,
+
+            @ApiParam(value = "The PISP TPP ID (otherwise referred to as the Client ID")
+            @RequestHeader(value = "x-ob-tpp-id", required = true) String tppId,
+
+            HttpServletRequest request,
+
+            Principal principal
+    ) throws OBErrorResponseException;
+
 }
--- secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/event/v3_1_10/eventsubscription/EventSubscriptionsApiController.java
+++ secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/event/v3_1_10/eventsubscription/EventSubscriptionsApiController.java
@@ -15,14 +15,223 @@
  */
 package com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.event.v3_1_10.eventsubscription;
 
+import static com.forgerock.sapi.gateway.ob.uk.common.datamodel.converter.event.FREventSubscriptionConverter.toFREventSubscriptionData;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.event.FREventSubscriptionData;
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBErrorResponseException;
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBRIErrorResponseCategory;
+import com.forgerock.sapi.gateway.ob.uk.common.error.OBRIErrorType;
+import com.forgerock.sapi.gateway.ob.uk.rs.server.common.util.VersionPathExtractor;
+import com.forgerock.sapi.gateway.ob.uk.rs.server.common.util.link.LinksHelper;
+import com.forgerock.sapi.gateway.ob.uk.rs.server.validator.ResourceVersionValidator;
+import com.forgerock.sapi.gateway.rs.resource.store.repo.entity.event.FREventSubscription;
 import com.forgerock.sapi.gateway.rs.resource.store.repo.mongo.events.EventSubscriptionsRepository;
 import com.forgerock.sapi.gateway.ob.uk.rs.obie.api.event.v3_1_10.eventsubscription.EventSubscriptionsApi;
+import com.forgerock.sapi.gateway.uk.common.shared.api.meta.obie.OBVersion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import uk.org.openbanking.datamodel.common.Links;
+import uk.org.openbanking.datamodel.common.Meta;
+import uk.org.openbanking.datamodel.event.OBEventSubscription1;
+import uk.org.openbanking.datamodel.event.OBEventSubscription1Data;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionResponse1;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionResponse1Data;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionsResponse1;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionsResponse1Data;
+import uk.org.openbanking.datamodel.event.OBEventSubscriptionsResponse1DataEventSubscription;
+
 @Controller("EventSubscriptionApiV3.1.10")
-public class EventSubscriptionsApiController extends com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.event.v3_1_9.eventsubscription.EventSubscriptionsApiController implements EventSubscriptionsApi {
+public class EventSubscriptionsApiController implements EventSubscriptionsApi {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    
+    private final EventSubscriptionsRepository eventSubscriptionsRepository;
 
     public EventSubscriptionsApiController(EventSubscriptionsRepository eventSubscriptionsRepository) {
-        super(eventSubscriptionsRepository);
+        this.eventSubscriptionsRepository = eventSubscriptionsRepository;
     }
+
+    @Override
+    public ResponseEntity<OBEventSubscriptionResponse1> createEventSubscription(
+            @Valid OBEventSubscription1 obEventSubscription,
+            String authorization,
+            String xJwsSignature,
+            String xFapiInteractionId,
+            String tppId,
+            HttpServletRequest request,
+            Principal principal
+    ) throws OBErrorResponseException {
+        logger.debug("Create new event subscriptions: {} for TPP: {}", obEventSubscription, tppId);
+
+        // Check if an event already exists for this TPP
+        Collection<FREventSubscription> byClientId = eventSubscriptionsRepository.findByTppId(tppId);
+        if (!byClientId.isEmpty()) {
+            logger.debug("An event subscription already exists for this TPP id: '{}' for the version: {}", tppId,
+                    byClientId.stream().findFirst().get());
+            throw new OBErrorResponseException(
+                    HttpStatus.CONFLICT,
+                    OBRIErrorResponseCategory.REQUEST_INVALID,
+                    OBRIErrorType.EVENT_SUBSCRIPTION_ALREADY_EXISTS.toOBError1()
+            );
+        }
+
+        // Persist the event subscription
+        FREventSubscription frEventSubscription = FREventSubscription.builder()
+                .id(UUID.randomUUID().toString())
+                .tppId(tppId)
+                .eventSubscription(toFREventSubscriptionData(obEventSubscription))
+                .build();
+        eventSubscriptionsRepository.save(frEventSubscription);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(packageResponse(frEventSubscription));
+    }
+
+    @Override
+    public ResponseEntity readEventSubscription(
+            String authorization,
+            String xFapiInteractionId,
+            String tppId,
+            HttpServletRequest request,
+            Principal principal
+    ) throws OBErrorResponseException {
+        logger.debug("Read event subscription for TPP: {}", tppId);
+
+        // A TPP must not access an event-subscription on an older version, via the EventSubscriptionId for an event-subscription created in a newer version
+        OBVersion apiVersion = VersionPathExtractor.getVersionFromPath(request);
+        Collection<FREventSubscription> frEventSubscriptions = Optional.ofNullable(eventSubscriptionsRepository.findByTppId(tppId))
+                .orElseGet(Collections::emptyList)
+                .stream()
+                .filter(frEventSubs -> ResourceVersionValidator.isAccessToResourceAllowed(apiVersion, OBVersion.fromString(frEventSubs.getEventSubscription().getVersion())))
+                .collect(Collectors.toList());
+        if (!frEventSubscriptions.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(packageResponse(frEventSubscriptions));
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("The event subscription can't be read via an older API version.");
+        }
+    }
+
+    @Override
+    public ResponseEntity updateEventSubscription(
+            String eventSubscriptionId,
+            @Valid OBEventSubscriptionResponse1 obEventSubscription,
+            String authorization,
+            String xJwsSignature,
+            String xFapiInteractionId,
+            String tppId,
+            HttpServletRequest request,
+            Principal principal
+    ) throws OBErrorResponseException {
+        OBEventSubscription1 updatedSubscription =
+                new OBEventSubscription1().data(new OBEventSubscription1Data()
+                        .callbackUrl(obEventSubscription.getData().getCallbackUrl())
+                        .eventTypes(obEventSubscription.getData().getEventTypes())
+                        .version(obEventSubscription.getData().getVersion())
+                );
+        Optional<FREventSubscription> byId = eventSubscriptionsRepository.findById(eventSubscriptionId);
+        if (byId.isPresent()) {
+            FREventSubscription existingEventSubscription = byId.get();
+            // A TPP must not update a event-subscription on an older version, via the EventSubscriptionId for an event-subscription created in a newer version
+            OBVersion apiVersion = VersionPathExtractor.getVersionFromPath(request);
+            OBVersion resourceVersion = OBVersion.fromString(existingEventSubscription.getEventSubscription().getVersion());
+            if (ResourceVersionValidator.isAccessToResourceAllowed(apiVersion, resourceVersion)) {
+                existingEventSubscription.setEventSubscription(toFREventSubscriptionData(updatedSubscription));
+                eventSubscriptionsRepository.save(existingEventSubscription);
+                return ResponseEntity.ok(packageResponse(existingEventSubscription));
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("The event subscription can't be update via an older API version.");
+            }
+        } else {
+            // PUT is only used for amending existing subscriptions
+            throw new OBErrorResponseException(
+                    HttpStatus.BAD_REQUEST,
+                    OBRIErrorResponseCategory.REQUEST_INVALID,
+                    OBRIErrorType.EVENT_SUBSCRIPTION_NOT_FOUND.toOBError1(eventSubscriptionId)
+            );
+        }
+    }
+
+    @Override
+    public ResponseEntity deleteEventSubscription(
+            String eventSubscriptionId,
+            String authorization,
+            String xFapiInteractionId,
+            String tppId,
+            HttpServletRequest request,
+            Principal principal
+    ) throws OBErrorResponseException {
+        final Optional<FREventSubscription> byId = eventSubscriptionsRepository.findById(eventSubscriptionId);
+        if (byId.isPresent()) {
+            // A TPP must not delete a event-subscription on an older version, via the EventSubscriptionId for an event-subscription created in a newer version
+            OBVersion apiVersion = VersionPathExtractor.getVersionFromPath(request);
+            OBVersion resourceVersion = OBVersion.fromString(byId.get().getEventSubscription().getVersion());
+            if (ResourceVersionValidator.isAccessToResourceAllowed(apiVersion, resourceVersion)) {
+                logger.debug("Deleting event subscriptions URL: {}", byId.get());
+                eventSubscriptionsRepository.deleteById(eventSubscriptionId);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("The event subscription can't be delete via an older API version.");
+            }
+        } else {
+            throw new OBErrorResponseException(
+                    HttpStatus.BAD_REQUEST,
+                    OBRIErrorResponseCategory.REQUEST_INVALID,
+                    OBRIErrorType.EVENT_SUBSCRIPTION_NOT_FOUND.toOBError1(eventSubscriptionId)
+            );
+        }
+    }
+
+    private OBEventSubscriptionsResponse1 packageResponse(Collection<FREventSubscription> eventSubs) {
+        List<OBEventSubscriptionsResponse1DataEventSubscription> eventSubsByClient = eventSubs.stream()
+                .map(e -> new OBEventSubscriptionsResponse1DataEventSubscription()
+                        .callbackUrl(e.getEventSubscription().getCallbackUrl())
+                        .eventSubscriptionId(e.getId())
+                        .eventTypes(e.getEventSubscription().getEventTypes())
+                        .version(e.getEventSubscription().getVersion())
+                ).collect(Collectors.toList());
+
+        if (eventSubsByClient.isEmpty()) {
+            return new OBEventSubscriptionsResponse1()
+                    .data(new OBEventSubscriptionsResponse1Data().eventSubscription(Collections.emptyList()))
+                    .meta(new Meta())
+                    .links(new Links());
+        } else {
+            return new OBEventSubscriptionsResponse1()
+                    .data(new OBEventSubscriptionsResponse1Data()
+                            .eventSubscription(eventSubsByClient))
+                    .meta(new Meta())
+                    .links(LinksHelper.createEventSubscriptionResourcesLink(this.getClass()));
+        }
+    }
+
+    private OBEventSubscriptionResponse1 packageResponse(FREventSubscription frEventSubscription) {
+        FREventSubscriptionData obEventSubs = frEventSubscription.getEventSubscription();
+        return new OBEventSubscriptionResponse1()
+                .data(new OBEventSubscriptionResponse1Data()
+                        .callbackUrl(obEventSubs.getCallbackUrl())
+                        .eventSubscriptionId(frEventSubscription.getId())
+                        .eventTypes(obEventSubs.getEventTypes())
+                        .version(obEventSubs.getVersion())
+                )
+                .links(LinksHelper.createEventSubscriptionSelfLink(this.getClass(), frEventSubscription.getId()))
+                .meta(new Meta());
+    }
+
 }
