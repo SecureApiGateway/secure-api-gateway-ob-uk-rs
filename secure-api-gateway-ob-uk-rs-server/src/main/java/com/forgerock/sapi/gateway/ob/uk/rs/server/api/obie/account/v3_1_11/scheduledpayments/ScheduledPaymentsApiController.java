59f4e115af3ca39b2b9252973ea6752d5302ef30
Laura Jianu <laura.jianu@forgerock.com>
2024-05-27 13:03:26 +0300

    1311: Adding OBIE v3.1.11 accounts API controllers
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1311

---
+++ secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/account/v3_1_11/scheduledpayments/ScheduledPaymentsApiController.java
@@ -0,0 +1,30 @@
+package com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.account.v3_1_11.scheduledpayments;
+
+import java.util.Optional;
+
+import javax.annotation.Generated;
+import javax.validation.constraints.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+@Controller
+@RequestMapping("${openapi.accountAndTransactionAPISpecification.base-path:/open-banking/v3.1/aisp}")
+public class ScheduledPaymentsApiController implements ScheduledPaymentsApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public ScheduledPaymentsApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
