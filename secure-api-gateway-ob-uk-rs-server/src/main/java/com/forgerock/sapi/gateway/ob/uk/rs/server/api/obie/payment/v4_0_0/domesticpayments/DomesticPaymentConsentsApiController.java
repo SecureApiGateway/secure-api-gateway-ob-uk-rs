1bd4ebb2f6daabd09ac04e2a2e6c35d3b61ec40f
Marian Tiris <marian.tiris@itsmartsystems.eu>
2024-10-15 12:54:32 +0300

    Update DomesticPaymentConsentsApiController.java

--- secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/v4_0_0/domesticpayments/DomesticPaymentConsentsApiController.java
+++ secure-api-gateway-ob-uk-rs-server/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/server/api/obie/payment/v4_0_0/domesticpayments/DomesticPaymentConsentsApiController.java
@@ -26,12 +26,13 @@ import com.forgerock.sapi.gateway.ob.uk.rs.server.api.obie.payment.factories.v4_
 import com.forgerock.sapi.gateway.ob.uk.rs.server.service.balance.FundsAvailabilityService;
 import com.forgerock.sapi.gateway.ob.uk.rs.server.v4.common.util.link.LinksHelper;
 //import com.forgerock.sapi.gateway.ob.uk.rs.validation.obie.OBValidationService;
-import com.forgerock.sapi.gateway.ob.uk.rs.validation.obie.OBValidationService;
+import com.forgerock.sapi.gateway.ob.uk.rs.validation.obie.v4.OBValidationService;
 import com.forgerock.sapi.gateway.rcs.consent.store.client.payment.domestic.v4_0_0.DomesticPaymentConsentStoreClient;
 import com.forgerock.sapi.gateway.rcs.consent.store.datamodel.payment.domestic.v4_0_0.CreateDomesticPaymentConsentRequest;
 import com.forgerock.sapi.gateway.rcs.consent.store.datamodel.payment.domestic.v4_0_0.DomesticPaymentConsent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -59,7 +60,7 @@ public class DomesticPaymentConsentsApiController implements DomesticPaymentCons
     private final OBWriteFundsConfirmationResponse1Factory fundsConfirmationResponseFactory;
 
     public DomesticPaymentConsentsApiController(DomesticPaymentConsentStoreClient consentStoreApiClient,
-                                                OBValidationService<OBWriteDomesticConsent4> domesticConsentValidator,
+                                                @Qualifier("v4.0.0OBValidationService")OBValidationService<OBWriteDomesticConsent4> domesticConsentValidator,
                                                 OBWriteDomesticConsentResponse5Factory consentResponseFactory,
                                                 FundsAvailabilityService fundsAvailabilityService,
                                                 OBWriteFundsConfirmationResponse1Factory fundsConfirmationResponseFactory) {
