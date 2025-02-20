4d4c12b459f44edb852e3cf9a0efa8ce5327320e
Wayne Morrison <wayne.morrison@forgerock.com>
2025-02-20 12:25:50 +0000

    1654 Bump SAPI-G parent and copyright years (RS)

--- secure-api-gateway-ob-uk-rs-validation/secure-api-gateway-ob-uk-rs-validation-obie/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/validation/obie/v4/payment/OBWriteFile2Validator.java
+++ secure-api-gateway-ob-uk-rs-validation/secure-api-gateway-ob-uk-rs-validation-obie/src/main/java/com/forgerock/sapi/gateway/ob/uk/rs/validation/obie/v4/payment/OBWriteFile2Validator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2024 ForgeRock AS (obst@forgerock.com)
+ * Copyright © 2020-2025 ForgeRock AS (obst@forgerock.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.forgerock.sapi.gateway.ob.uk.rs.validation.obie.v4.payment;
 
 import com.forgerock.sapi.gateway.ob.uk.rs.validation.ValidationResult;
